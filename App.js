49601851aeae5ecebd6236fbe4fc29f8d5b27787
witoldolszyk <30417355+witoldolszyk@users.noreply.github.com>
2018-01-28 21:27:29 +0100

    Add files via upload

---
+++ App.js
@@ -0,0 +1,36 @@
+// OGÓLNA FUNKCJA
+
+// ZMIENNE
+	var baseUrl = 'https://kodilla.com/pl/bootcamp-api';
+	var myHeaders = {
+	  'X-Client-Id': '2315',
+	  'X-Auth-Token': 'caafbea0f629d5011743e93031f7bb36'
+	};
+
+// KONFIGURACJA NAGŁÓWKÓW DO ZAPYTAŃ
+	$.ajaxSetup({
+		headers: myHeaders
+	});
+
+	$.ajax({
+		url: baseUrl + '/board',
+		method: 'GET',
+		success: function(response) {
+			setupColumns(response.columns);
+		}
+	});
+
+	function setupColumns(columns) {
+		columns.forEach(function (column) {
+			var col = new Column(column.id, column.name);
+			board.createColumn(col);
+			setupCards(col, column.cards);
+		});
+	}
+
+	function setupCards(col, cards) {
+		cards.forEach(function (card) {
+			var card = new Card(card.id, card.name, card.bootcamp_kanban_column_id);
+			col.createCard(card);
+		});
+	}
